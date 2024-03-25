637fc1f9974bbce1d3f8cd728222ef7f9c1e9e56
Jonathan Suru <jonathansuru@icloud.com>
2024-03-25 19:20:07 +0100

    refactor: Update dependencies for the project
    - Updated mlx-lm to version 0.5.0
    - Added streamlit version 1.32.2, pypdf2 version 2.10.5, and python-docx version 1.1.0 for improved functionality.
    - Introduced new functions for reading DOCX and PDF files.
    - Added new code for creating flashcards and displaying metrics in the dashboard.
    - Modified the layout of the responsive menu for better navigation.

---
+++ median/file_reader.py
@@ -0,0 +1,37 @@
+import PyPDF2
+from docx import Document
+
+
+def read_docx(docx_file):
+    # Create a Document object
+    doc = Document(docx_file)
+    # Extract the text
+    content = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+    return content
+
+
+def read_pdf(pdf_path):
+    # Read the PDF file
+    pdf_reader = PyPDF2.PdfFileReader(pdf_path)
+    # Extract the content
+    content = ""
+    for page in range(pdf_reader.getNumPages()):
+        content += pdf_reader.getPage(page).extractText()
+
+    return content
+
+
+def main(file, file_type):
+    if file_type == "text/markdown":
+        return file.read().decode()
+    elif file_type == "application/pdf":
+        return read_pdf(file)
+    elif (
+        file_type
+        == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ):
+        return read_docx(file)
+    elif file_type == "text/plain":
+        return file.read().decode()
+    else:
+        return None
