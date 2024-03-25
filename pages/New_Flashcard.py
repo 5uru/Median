8b60c5ad6501f53fcf9e6f3669ee306be9a55d41
Restyled.io <commits@restyled.io>
2024-03-25 18:24:50 +0000

    Restyled by yapf

--- pages/New_Flashcard.py
+++ pages/New_Flashcard.py
@@ -14,7 +14,8 @@ st.title("Create New Flashcard")
 with st.form("my_form"):
     flashcard_name = st.text_input("Flashcard Name")
 
-    data = st.file_uploader("Upload data file", type=["pdf", "docx", "md", "txt"])
+    data = st.file_uploader("Upload data file",
+                            type=["pdf", "docx", "md", "txt"])
 
     submit = st.form_submit_button("Create Flashcard")
     if submit:
